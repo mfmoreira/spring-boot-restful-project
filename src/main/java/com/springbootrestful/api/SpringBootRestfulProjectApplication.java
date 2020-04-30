583a63cb7af20493512984dd1548df96ce3c79eb
mfmoreira <mfmoreira7@gmail.com>
2020-04-30 00:29:24 -0300

    Commit initial

---
+++ src/main/java/com/springbootrestful/api/SpringBootRestfulProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springbootrestful.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootrestful/api/SpringBootRestfulProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootrestful.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
